d9dcfafeabfe98d7639d62ba75043c3f21afd72f
Vignesh <50480826+vigneshthirumkce@users.noreply.github.com>
2020-08-05 10:46:30 +0530

    Add files via upload

---
+++ permutations.py
@@ -0,0 +1,24 @@
+from itertools import permutations as p
+x,y = input().split()
+in1=int(y)
+for i in p(sorted(x),in1):
+    print("".join(i))
+
+
+
+ #input
+ #HACK 2
+ 
+ #output
+ #AC
+ #AH
+ #AK
+ #CA
+ #CH
+ #CK
+ #HA
+ #HC
+ #HK
+ #KA
+ #KC
+ #KH
